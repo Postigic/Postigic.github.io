c6c7fdcc4bea63486f4657d5c02a78812e87c374
Postigic <143212308+Postigic@users.noreply.github.com>
2024-12-16 15:12:28 +0800

    update...... i really need to keep working on it huh?

---
+++ script.js
@@ -0,0 +1,37 @@
+fetch("data/skills.json")
+    .then((response) => response.json())
+    .then((data) => generateSkills(data));
+
+function generateSkills(data) {
+    const skillsSection = document.querySelector(".skills");
+
+    for (const category in data.skills) {
+        const skillCategory = document.createElement("div");
+        skillCategory.classList.add("skill-category");
+
+        skillCategory.innerHTML = `<h3>${category}</h3>`;
+        const skillsContainer = document.createElement("div");
+        skillsContainer.classList.add("skills-container");
+
+        const sortedSkills = data.skills[category].sort((a, b) =>
+            a.name.localeCompare(b.name)
+        );
+
+        sortedSkills.forEach((skill) => {
+            const skillItem = document.createElement("div");
+            skillItem.classList.add("skill-item");
+
+            skillItem.innerHTML = `
+              <div class="icon">
+                <i class="${skill.icon}" style="color: ${skill.color}"></i>
+              </div>
+              <p>${skill.name}</p>
+            `;
+
+            skillsContainer.appendChild(skillItem);
+        });
+
+        skillCategory.appendChild(skillsContainer);
+        skillsSection.appendChild(skillCategory);
+    }
+}
